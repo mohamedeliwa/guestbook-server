4895cd2409c0c2cebebe04d43305cc113933b4e6
mohamedeliwa <mohamed.av11@gmail.com>
2020-09-20 00:25:24 +0200

    installed and applies CORS for local testing purposes

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 require("./db/mongoose");
 const userRouter = require("./routers/user");
 const messageRouter = require("./routers/message");
@@ -9,9 +10,17 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
+/**
+ * Enabling CORS for local testing purposes.
+ * to be disabled on production
+ */
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true,
+};
+app.use(cors(corsOptions));
+
 app.use(userRouter);
 app.use(messageRouter);
 
-
 module.exports = app;
-
